5b6c2dfbc530768404a8da74e11f83f21603a60a
Masaltsev <a.o.masaltsev@gmail.com>
2022-05-07 18:49:45 +0300

    init

---
+++ src/test/java/ua/masaltsev/webnotes/WebNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.masaltsev.webnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
