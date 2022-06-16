3d41deffe7d1ad00e3cb040bf5eb552ccd10c055
Masaltsev <a.o.masaltsev@gmail.com>
2022-06-16 22:10:50 +0300

    h2 database used

--- src/main/java/ua/masaltsev/webnotes/entity/Note.java
+++ src/main/java/ua/masaltsev/webnotes/entity/Note.java
@@ -1,36 +1,39 @@
 package ua.masaltsev.webnotes.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.*;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "notes")
 public class Note {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
+
     private String title;
     private String contents;
 
-    public Note() {
+    public Integer getId() {
+        return this.id;
     }
 
-    public Note(String title, String contents) {
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getContents() {
+        return this.contents;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
         this.title = title;
-        this.contents = contents;
     }
 
-    @Override
-    public String toString() {
-        return "Note{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", contents='" + contents + '\'' +
-                '}';
+    public void setContents(String contents) {
+        this.contents = contents;
     }
 }
