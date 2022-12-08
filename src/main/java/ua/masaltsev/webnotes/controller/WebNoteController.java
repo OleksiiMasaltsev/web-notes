464c769cd1795aa53da5e2f0893df26b502e0b30
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-12-08 19:06:28 +0200

    review

--- src/main/java/ua/masaltsev/webnotes/controller/WebNoteController.java
+++ src/main/java/ua/masaltsev/webnotes/controller/WebNoteController.java
@@ -50,6 +50,4 @@ public class WebNoteController {
         service.save(note);
         return "redirect:/showAllNotes";
     }
-
-
 }
--- src/main/java/ua/masaltsev/webnotes/entity/Note.java
+++ src/main/java/ua/masaltsev/webnotes/entity/Note.java
@@ -5,11 +5,9 @@ import javax.persistence.*;
 @Entity
 @Table(name = "notes")
 public class Note {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     private String title;
     private String contents;
 
